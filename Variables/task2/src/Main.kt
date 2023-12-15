1393c145bc78a766a21a66d2478b5060830dd1f6
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2023-12-15 17:19:25 +0200

    feat: add task2 for Variables lesson

---
+++ Variables/task2/src/Main.kt
@@ -0,0 +1,15 @@
+// type your solution here
+
+fun main() {
+
+    var number = 70
+//
+    number = 40
+
+    number = number + 40
+
+    // TODO: Make the necessary changes to achieve an output of 70.
+
+    print(number)
+
+}
---
+++ Variables/task2/task.md
@@ -0,0 +1,7 @@
+## Todo
+
+Make the necessary changes to achieve an output of 70.
+
+<div class="hint">
+This task has multiple solutions, don't use the direct one :D
+</div>
